7f57b65826e786edc9393b61b4c023083db9066b
adwaithkm1 <adwaithkm952@gmail.com>
2025-03-20 10:28:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // Important for deployment
   server: {
-    host: "::",
     port: 8080,
+    host: "0.0.0.0", // Ensures Render can bind to the correct IP
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
